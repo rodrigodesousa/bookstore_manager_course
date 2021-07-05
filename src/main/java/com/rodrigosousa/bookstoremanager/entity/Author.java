23bb5437a517df4fd8af7c0ab60fc6d1817853d3
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-07-05 19:38:31 -0400

    Developed project entities

---
+++ src/main/java/com/rodrigosousa/bookstoremanager/entity/Author.java
@@ -0,0 +1,22 @@
+package com.rodrigosousa.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
