eaa808c85b575030d49dfb173d466050d9469dac
Rodrigo Sousa <rodrigudesousa@gmail.com>
2021-07-05 23:35:08 -0400

    unit test for find by id service

---
+++ src/test/java/com/rodrigosousa/bookstoremanager/service/BookServiceTest.java
@@ -0,0 +1,42 @@
+package com.rodrigosousa.bookstoremanager.service;
+
+import com.rodrigosousa.bookstoremanager.dto.BookDTO;
+import com.rodrigosousa.bookstoremanager.entity.Book;
+import com.rodrigosousa.bookstoremanager.repository.BookRepository;
+import com.rodrigosousa.bookstoremanager.utils.BookUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static com.rodrigosousa.bookstoremanager.utils.BookUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    void whenGivenExistingIdThenReturnThisBook() {
+        Book expectedFoundBook = BookUtils.createFakeBook();
+
+        Mockito.when(bookRepository.findById(expectedFoundBook.getId())).thenReturn(Optional.of(expectedFoundBook));
+
+        BookDTO bookDTO = bookService.findById(expectedFoundBook.getId());
+
+        Assertions.assertEquals(expectedFoundBook.getName(), bookDTO.getName());
+        Assertions.assertEquals(expectedFoundBook.getIsbn(), bookDTO.getIsbn());
+        Assertions.assertEquals(expectedFoundBook.getPublisherName(), bookDTO.getPublisherName());
+    }
+}
